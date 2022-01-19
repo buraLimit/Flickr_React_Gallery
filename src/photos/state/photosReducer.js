44742c616345281e2736317e8f9e51c56fd82bf3
Stefan Bura <stefannrt818@gs.viser.edu.rs>
2022-01-19 23:51:44 +0100

    MVP

---
+++ src/photos/state/photosReducer.js
@@ -0,0 +1,16 @@
+import initialState from "../../state/reducers/initialState";
+import * as types from "./actionTypes";
+
+export default function photosReducer(state = initialState.photos, action) {
+  switch (action.type) {
+    case types.GET_RECENT_PHOTOS:
+      return action.payload;
+    case types.GET_SEARCHED_PHOTOS: {
+      console.log("OVO JE");
+      console.log(action.type);
+      return action.payload;
+    }
+    default:
+      return state;
+  }
+}
